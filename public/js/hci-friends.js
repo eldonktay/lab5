9d7ebcc2f31be2af95df56985681b09961f81eaa
Eldon Tay <ektay@ucsd.edu>
2019-02-07 04:42:28 -0800

    finished

--- public/js/hci-friends.js
+++ public/js/hci-friends.js
@@ -10,6 +10,13 @@ $(document).ready(function() {
  */
 function initializePage() {
 	console.log("Javascript connected!");
+	$(".friends a h3").click(projectClick);
+}
+
+function projectClick(e) {
+	e.preventDefault();
+	var name = $(this).text();
+	$(this).text(anagrammedName(name));
 }
 
 function anagrammedName(name) {
--- routes/add.js
+++ routes/add.js
@@ -1,5 +1,12 @@
 var data = require("../data.json");
 
 exports.addFriend = function(request, response) {    
-	// Your code goes here
+	var name = request.query.name;
+    var desc = request.query.description;
+    var image = "http://lorempixel.com/400/400/people";
+    var entry = {"name": name, "description": desc, "imageURL": image};
+    
+    console.log(entry);
+    data.friends.push(entry);
+    response.render('index', data);
  }
